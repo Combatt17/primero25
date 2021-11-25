53d3208aa666725ef674839f70c5f58de5d7648c
Carlos <carloscombatt17@gmail.com>
2021-11-25 17:51:06 -0500

    push

--- src/app/consumo/equipo/equipo.component.ts
+++ src/app/consumo/equipo/equipo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Equipo } from 'src/app/models/equipo.model';
+
+//Importo el modelo
 import { EquipoService } from 'src/app/services/equipo.service';
 
 @Component({
@@ -8,19 +11,37 @@ import { EquipoService } from 'src/app/services/equipo.service';
 })
 export class EquipoComponent implements OnInit {
 
+  //Array que contiene la información traida del servidor
+  equipos:Equipo[] = [];
+
+  dato = {
+    "id":"13456",
+    "nombre":"Bogotá"
+  }
+
+
   //inyectar el servicio creado
   constructor(private serviceEquipo:  EquipoService) { }
 
   ngOnInit(): void {
     this.traerEquipos();
+    console.log(this.dato)
+    this.guardarEquipos()
   }
 
   //crear un método que me ejecute el service
   traerEquipos(){
-    this.serviceEquipo.traerEquipos().subscribe(data =>{
-      console.log(data);
+    this.serviceEquipo.traerEquipos().subscribe((data: any)=>{
+      this.equipos=data
     })
   }
 
+  guardarEquipos(){
+    this.serviceEquipo.enviarEquipo(this.dato).subscribe((data: any)=>{
+      
+    console.log(data)
+
+    })
+  }
 
 }
--- src/app/services/equipo.service.ts
+++ src/app/services/equipo.service.ts
@@ -1,14 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environment';
+import { Equipo } from '../models/equipo.model';
 @Injectable({
   providedIn: 'root'
 })
 export class EquipoService {
 
+  url: string=environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   traerEquipos(){
-    return this.http.get('http://localhost:8080/api/equipos');
+    return this.http.get(`${this.url}/api/equipos`);
   }
+
+  enviarEquipo(dato:any){
+    return this.http.post(`${this.url}/api/equipos`, dato)
+  }
+
+
+
+ 
+  
 }
