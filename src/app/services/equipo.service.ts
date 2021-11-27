7e9bc7846a16336157080c28b4f43d6fd582ef84
Carlos <carloscombatt17@gmail.com>
2021-11-27 10:27:05 -0500

    Formulario

--- src/app/services/equipo.service.ts
+++ src/app/services/equipo.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Equipo } from '../models/equipo.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +8,7 @@ export class EquipoService {
 
   url: string=environment.baseUrl;
 
+  //inyecto el cliente http
   constructor(private http: HttpClient) { }
 
   traerEquipos(){
